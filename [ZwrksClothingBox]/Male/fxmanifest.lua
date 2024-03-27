8017490f0bc9063e41fb17fe876ec659aaba5b2d
Jaxx | Zwrks 5M <161543178+Zwrks5M@users.noreply.github.com>
2024-03-27 05:41:04 -0700

    Add files via upload

---
+++ [ZwrksClothingBox]/Male/fxmanifest.lua
@@ -0,0 +1,11 @@
+-- Created By @ZWRKS
+
+fx_version 'cerulean'
+
+games   { 'gta5' }
+author 'https://discord.gg/zwrks'
+description 'Basic Clothing Directory By discord.gg/ZWRKS'
+
+description 'Clothing'
+
+lua54 'yes'
